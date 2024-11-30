9bd296718b4b7c7f9bdba6a8a03a54452ea3c57f
Leonardo Silva <leonardo.silva@adaptivesoft.com.br>
2024-11-30 18:51:59 -0300

    moving Handler function to server.go and deleting index.go

--- api/server.go
+++ api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"emailsync/config"
 	"emailsync/controller"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -24,7 +25,7 @@ func (api *API) LoadDefault() *API {
 	api.HideBanner = true
 	api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Output: log.StandardLogger().Out,
 		CustomTimeFormat: "2006-01-02 15:04:05.00000",
-		Format: `[${time_custom}] [ECHO ] [api/index.go:0] [id=${id}] [remote_ip=${remote_ip}] ` +
+		Format: `[${time_custom}] [ECHO ] [api/server.go:0] [id=${id}] [remote_ip=${remote_ip}] ` +
 			`[status=${status}] [error=${error}] [protocol=${protocol}] [agent=${user_agent}] ` +
 			`[latency=${latency}] [latency_human=${latency_human}] [bytes_in=${bytes_in}] ` +
 			`[bytes_out=${bytes_out}] – ${method} ${host}${uri}` + "\n"}))
@@ -45,4 +46,14 @@ func (api *API) StartAPI() {
 	if err := api.Start(api.Server.Addr); err != nil {
 		log.Error(err)
 	}
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	log.Info("Starting web server on vercel...")
+	api := &API{Echo: echo.New()}
+	log.Info("[Success] Web server started on vercel.")
+	api.LoadDefault().StartAPI()
+	api.ServeHTTP(w, r)
+
 }
