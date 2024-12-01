2f7aa186cf0c69343e8ee24808c7d85d69c9b094
Leonardo Silva <leonardo.silva@adaptivesoft.com.br>
2024-12-01 20:28:35 -0300

    trying to serve swagger doc

--- doc/swagger-initializer.js
+++ doc/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./emailsync.json",
+    url: "./doc/emailsync.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
